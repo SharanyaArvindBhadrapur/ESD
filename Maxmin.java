a83a7c51932968a8ed2a6501576882fd284d8856
Sharanya <sha.sharu97@gmail.com>
2021-03-17 18:55:53 +0530

    maxmin

---
+++ Maxmin.java
@@ -0,0 +1,10 @@
+class Maxmin{
+	static int a=16, b=36;
+
+	public static void main(String[] args) {
+		if(a>b)
+			System.out.println("Maximum number is "+a + " and Minimum number is "+b);
+		else
+			System.out.println("Maximum number is "+b + " and Minimum number is "+a);
+	}
+}
