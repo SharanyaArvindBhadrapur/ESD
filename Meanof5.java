71750d1ed3c0444a9404961b5801190be456fe8a
Sharanya <sha.sharu97@gmail.com>
2021-03-17 18:58:24 +0530

    mean

---
+++ Meanof5.java
@@ -0,0 +1,10 @@
+class Meanof5{
+	static int a=5, b=10, c=15, d=20, e=25;
+	static int m;
+
+	public static void main(String[] args) 
+	{
+    m = (a+b+c+d+e)/5;
+    System.out.println("Mean of 5 numbers: "+m);
+	}
+}
