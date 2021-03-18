5ac6cb6fce76d1248789f9b8db44a865d107b589
Sharanya <sha.sharu97@gmail.com>
2021-03-18 19:29:43 +0530

    ascii

---
+++ Ascii.java
@@ -0,0 +1,11 @@
+class Ascii{
+    
+	static char c = 'A';
+	static int i = c;
+	    
+    public static void main(String[] args) {
+
+    	System.out.println(" ASCII value of "+c + " is "+i);
+    	
+    }
+}
