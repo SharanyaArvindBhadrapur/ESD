e837c560432175a4dea28c2c0f5966717ca41f1a
Sharanya <sha.sharu97@gmail.com>
2021-03-18 14:47:16 +0530

    sqrt

---
+++ Sqrt.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+class Sqrt{
+	static float a=25;
+	
+	public static void main(String[] args) {
+    System.out.println(" Square root of "+a + " is "+Math.sqrt(a));	
+	}
+
+	
+}
