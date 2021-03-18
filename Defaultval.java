a024ba184a1ba8e2d3618e41807e3ab23e4f6be7
Sharanya <sha.sharu97@gmail.com>
2021-03-18 17:20:35 +0530

    default value

---
+++ Defaultval.java
@@ -0,0 +1,17 @@
+class Defaultval{
+
+	static int k;
+	static double d;
+	static float f;
+	static boolean istrue;
+	static String p;
+
+	public static void main(String[] args) {
+		
+		System.out.println("int default value = "+ k);
+		System.out.println("double default value = "+ d);
+		System.out.println("float default value = "+ f);
+		System.out.println("boolean default value = "+ istrue);
+		System.out.println("String default value = "+ p);
+	}
+}
