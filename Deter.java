4043f7a863303d539ad625eb9cd6df4438c923c3
Sharanya <sha.sharu97@gmail.com>
2021-03-19 11:36:54 +0530

    Determinant

---
+++ Deter.java
@@ -0,0 +1,12 @@
+class Deter{
+
+	static int a = 6, b = 10, c = -1;
+	static int d;
+
+	public static void main(String[] args) {
+
+		d = (b * b)-(4 *a * c);
+		System.out.println("Determinant of the equation is "+d);
+	
+	}
+}
