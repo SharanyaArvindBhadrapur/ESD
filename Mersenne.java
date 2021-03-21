88e72fc1941576f5d1ce3f2a54fd37ca37fb84c9
Sharanya <sha.sharu97@gmail.com>
2021-03-22 02:52:38 +0530

    mersenne number

---
+++ Mersenne.java
@@ -0,0 +1,41 @@
+import java.lang.Math;
+class Mersenne{
+
+	int a;
+
+	void circ()
+	{
+		double c=0, i=0;
+		//a = x;
+		for(int j=1; j<10; j++)
+		    {
+		    	while(i<j)
+		            {
+			            for(i=1; i<j; i++)
+		                    {
+			                    if((j%i)==0)
+			                        {
+				                        c++;
+			                        }
+		                    }
+		                    //System.out.println(+c);
+		                    if(c==1)
+		                    {
+		                    	//System.out.println("Prime "+i);
+		                    	double x = Math.pow(2,i);
+		                    	double y = x-1;
+		                    	System.out.println("Prime "+y);
+		                    }			                
+		                    c = 0;
+		            }
+
+		    }
+
+	}
+
+	public static void main(String[] args) {
+		
+		Mersenne obj = new Mersenne();
+		obj.circ();
+	}
+}
